1e9cf203baf9784bb8778c35b3b62863a1324b77
Adam Herstein <adam.herstein@nwea.org>
2017-09-11 12:44:01 -0700

    Added logic to check alerts.

--- alerts.py
+++ alerts.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python
-import requests
+from google.transit import gtfs_realtime_pb2
+import urllib
+import unicodedata
 import appid
 
-ENDPOINT = "developer.trimet.org/ws/V1/FeedSpecAlerts?appID={0}".format(appid.APP_ID)
+ENDPOINT = "developer.trimet.org/ws/V1/FeedSpecAlerts?appID={0}"
 
 
 def get_alerts():
-    r = requests.get('https://' + ENDPOINT)
-    data = r.content
+    # Get feed data
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = urllib.urlopen('https://' + ENDPOINT.format(appid.APP_ID))
+    feed.ParseFromString(response.read())
 
-    if 'error' in data:
-        print(data['error'])
-        exit(1)
+    # Loop through ang get all alerts
+    for entity in feed.entity:
+        if entity.HasField('alert'):
+            alerts = entity.alert.description_text.translation  # Path to array of alerts
+            for alert in alerts:  # Loop though array of alerts
+                # do_something(alert.text)
+                print(alert)
+                print("-----------------------------------------------------------------------------------------------")
 
-    print(data)
+
+def do_something(alert):
+    alert = unicodedata.normalize('NFKD', alert).encode('ascii', 'ignore')
+    if 'auto blocking' in alert or 'car blocking' in alert:
+        print(alert)
 
 
 get_alerts()
