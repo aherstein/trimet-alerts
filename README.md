46011e9ab1a251037ef493be68142c887fc1f810
Adam Herstein <adam.herstein@nwea.org>
2017-09-11 14:02:48 -0700

    Added code to tweet out alert.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Create `appid.py` and store your TriMet app ID in there:
+Create `secrets.py` and store your TriMet app ID, and Twtter creds in there:
 ```
 APP_ID = "1234567890ABCDEF"
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
 ```
--- alerts.py
+++ alerts.py
@@ -2,15 +2,26 @@
 from google.transit import gtfs_realtime_pb2
 import urllib
 import unicodedata
-import appid
+import oauth2
+import json
+import secrets
 
-ENDPOINT = "developer.trimet.org/ws/V1/FeedSpecAlerts?appID={0}"
+TRIMET_ALERTS_ENDPOINT = "developer.trimet.org/ws/V1/FeedSpecAlerts?appID={0}"
+TWITTER_POST_ENDPOINT = "https://api.twitter.com/1.1/statuses/update.json?status={0}"
+
+
+def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
+    consumer = oauth2.Consumer(key=secrets.CONSUMER_KEY, secret=secrets.CONSUMER_SECRET)
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers )
+    return content
 
 
 def get_alerts():
     # Get feed data
     feed = gtfs_realtime_pb2.FeedMessage()
-    response = urllib.urlopen('https://' + ENDPOINT.format(appid.APP_ID))
+    response = urllib.urlopen('https://' + TRIMET_ALERTS_ENDPOINT.format(secrets.APP_ID))
     feed.ParseFromString(response.read())
 
     # Loop through ang get all alerts
@@ -18,15 +29,22 @@ def get_alerts():
         if entity.HasField('alert'):
             alerts = entity.alert.description_text.translation  # Path to array of alerts
             for alert in alerts:  # Loop though array of alerts
-                # do_something(alert.text)
-                print(alert)
-                print("-----------------------------------------------------------------------------------------------")
+                tweet(alert.text)
 
 
-def do_something(alert):
+def tweet(alert):
     alert = unicodedata.normalize('NFKD', alert).encode('ascii', 'ignore')
     if 'auto blocking' in alert or 'car blocking' in alert:
         print(alert)
+        r = oauth_req(TWITTER_POST_ENDPOINT.format(urllib.quote_plus(alert)),
+                      secrets.ACCESS_TOKEN,
+                      secrets.ACCESS_SECRET,
+                      "POST")
+        data = json.loads(r)
+
+        if 'errors' in data:
+            print(data['errors'][0]['message'])
+            exit(1)
 
 
 get_alerts()
